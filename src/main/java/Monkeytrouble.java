d5d6e7c5ff85e318b589730708fe29286b9db546
OlenaParkhom <opa@compent.net>
2021-06-09 16:56:11 +0200

    test

---
+++ src/main/java/Monkeytrouble.java
@@ -0,0 +1,13 @@
+public class Monkeytrouble { //Warmup1/monkeyTrouble
+
+    public boolean monkeyTrouble(boolean aSmile, boolean bSmile) {
+        if (aSmile && bSmile) {
+            return true;
+        }
+        if (!aSmile && bSmile){
+            return true;
+        }
+        else
+            return false;
+    }
+}
---
+++ src/main/java/Sleepin.java
@@ -0,0 +1,13 @@
+public class Sleepin { //Warmup-1/sleepIn
+    public static void main(String[] args) {
+
+    }
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+
+        return false;
+
+    }
+}
