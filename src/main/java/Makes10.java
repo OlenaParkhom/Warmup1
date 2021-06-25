0791708a3d0a1cbfe8f82a289ce745a67399495b
OlenaParkhom <85258978+OlenaParkhom@users.noreply.github.com>
2021-06-25 09:05:48 +0200

    Add files via upload

---
+++ src/main/java/Makes10.java
@@ -0,0 +1,16 @@
+public class Makes10 {
+    public static void main(String[] args) {
+        makes10();
+        int a = 5;
+        int b = 6;
+    }
+
+    public boolean makes10(int a, int b){
+        return (a==10 || b==10 || a+b==10);
+    }
+
+    }
+
+
+
+
